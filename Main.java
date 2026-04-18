decdee61a15654eb81b75f1ff4a6ddc60defd178
Jddubb <wintjustin14@gmail.com>
2026-04-18 01:25:28 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel(10); // You can change the number of rooms here
+        hotel.run(); // Start the hotel booking system
+    }
+}
+
+
+
+      
